02af0c05ca342fbfb96c498fdf7714b3438fb718
Sijoon Lee <shijoonlee@gmail.com>
2020-07-13 07:23:57 -0700

    boilerplate

---
+++ frontend/index.js
@@ -0,0 +1,11 @@
+import ReactDOM from "react-dom";
+import React, {useRef} from "react";
+
+
+const AppView = () => {
+    return (
+        <div>test</div>
+    )
+}
+
+ReactDOM.render(<AppView/>, document.getElementById("app"));
